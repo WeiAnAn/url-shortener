4b8068ef4ac577c8e0cf99abeddcecebddb5eae9
Wei-An, Yen <j22461615@gmail.com>
2023-05-28 17:56:54 +0800

    feat: add RealTime

--- internal/utils/time.go
+++ internal/utils/time.go
@@ -5,3 +5,9 @@ import "time"
 type TimeUtil interface {
 	Until(t time.Time) time.Duration
 }
+
+type RealTime struct{}
+
+func (rt *RealTime) Until(t time.Time) time.Duration {
+	return time.Until(t)
+}
